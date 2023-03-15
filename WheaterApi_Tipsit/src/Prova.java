d82ad37314880061da126aa5cefb5330dc4c4070
Biasuzzi Davide <biasuzzi.davide@gmail.com>
2023-03-15 15:50:53 +0100

    Aggiunta classe
    Ho aggiunto una classe di prova (Prova.java) che fa una richiesta alla nostra api e che stampa a video il documento xml risultante

---
+++ WheaterApi_Tipsit/src/Prova.java
@@ -0,0 +1,30 @@
+	import java.net.HttpURLConnection;
+	import java.net.URL;
+	import java.io.BufferedReader;
+	import java.io.InputStreamReader;
+public class Prova {
+
+	    public static void main(String[] args) {
+	        try {
+	            // Crea l'URL dell'API e apre una connessione HTTP
+	            URL url = new URL("http://api.weatherapi.com/v1/current.xml?key=4dcd56b9af624eaaa11132617231503&q=Nervesa&aqi=no\r\n"
+	            		+ "");
+	            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	            conn.setRequestMethod("GET");
+
+	            // Legge il contenuto della risposta HTTP
+	            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	            String inputLine;
+	            StringBuilder response = new StringBuilder();
+	            while ((inputLine = in.readLine()) != null) {
+	                response.append(inputLine);
+	            }
+	            in.close();
+
+	            // Stampa il documento XML ricevuto dalla risposta HTTP
+	            System.out.println(response.toString());
+	        } catch (Exception e) {
+	            System.err.println("Errore durante la richiesta XML: " + e.getMessage());
+	        }
+	    }
+}
