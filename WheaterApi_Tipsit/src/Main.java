ab2af525265e1dbbb3c4106bd1da684a228411f1
elparo290 <parovelfilippo1@gmail.com>
2023-03-15 15:35:47 +0100

    normale
    Ritorno normalità

--- WheaterApi_Tipsit/src/Main.java
+++ WheaterApi_Tipsit/src/Main.java
@@ -1,14 +1,14 @@
 
 
 
-//Chiave API 
+//Chiave API
 //4dcd56b9af624eaaa11132617231503
  
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prova");
+		
 	}
 
 }
